a0f32b07bd1af9cf9f680d6c9abf94da74a92d5f
ne0d34th <ne0d34th@users.noreply.github.com>
2016-02-13 01:16:19 +0700

    Update SSAParse.py

--- SSAParse.py
+++ SSAParse.py
@@ -1,19 +1,23 @@
-#!/usr/bin/env python
-
-# SSA Subtitle file parser by ne0d34th
-# should be still buggy
-# feel free to copy as long credit is given
+#!/usr/bin/env python2
 
 class SSAParse(object):
 	__file = None
 	__styles = []
 	__subtitle = []
 
+	__styleFormat = []
+	__subFormat = []
+
+	# time format
 	IN_SECONDS = 1
 
+	#color format
+	HEXADECIMAL = 1
+	DECIMAL = 2
+
 	def __init__(self, file):
-		self.__file = open(file, "r")
-		content = self.__file.read()
+		with open(file, "r") as self.__file:
+			content = self.__file.read()
 
 		style = content.split("[V4 Styles]\n")[1]
 		style = style.split("[Events]")[0]
@@ -26,6 +30,8 @@ class SSAParse(object):
 		listProp = []
 		for properties in getFormats:
 			listProp.append(properties.strip())
+
+		self.__styleFormat = listProp
 
 		styleListRawArray = styleListRaw.split("\n")
 		for styleOne in styleListRawArray:
@@ -49,6 +55,8 @@ class SSAParse(object):
 		for properties in getFormats:
 			listProp.append(properties.strip())
 
+		self.__subFormat = listProp
+
 		subListRawArray = subListRaw.split("\n")
 		for subOne in subListRawArray:
 			if(subOne==""):
@@ -65,3 +73,111 @@ class SSAParse(object):
 
 	def getStyle(self):
 		return self.__styles
+
+	def getSubtitleFormatted(self,*args):
+		styleFormat = []
+		subFormat = []
+		
+		for searchFormat in args:
+			found = False
+			for styleFormatOne in self.__styleFormat:
+				if(searchFormat==styleFormatOne):
+					styleFormat.append(styleFormatOne)
+					found = True
+					break
+
+			if(found==True):
+				continue
+
+			for subFormatOne in self.__subFormat:
+				if(searchFormat==subFormatOne and searchFormat!="Name"):
+					subFormat.append(subFormatOne)
+					found = True
+					break
+
+			if(found==False):
+				raise ValueError("Format Name '"+searchFormat+"' not Found!")
+
+		formattedSub = []
+
+		for sub in self.__subtitle:
+			subOne = {}
+			for subFOne in subFormat:
+				subOne[subFOne] = sub[subFOne]
+			if(len(styleFormat)!=0):
+				styleName = sub["Style"]
+				styleIndex = self.__getStyleIndex(styleName)
+				for styleOne in styleFormat:
+					subOne[styleOne] = self.__styles[styleIndex][styleOne]
+			formattedSub.append(subOne)
+		return formattedSub
+
+	def convertTime(self, type):
+		if(type==self.IN_SECONDS):
+			for x in xrange(0, len(self.__subtitle)):
+				timeStart = SSAParse.parseTime(self.__subtitle[x]["Start"])
+				timeStartNew = float(timeStart["hours"]*3600)+float(timeStart["minutes"]*60)+timeStart["seconds"]
+				self.__subtitle[x]["Start"] = timeStartNew
+
+				timeEnd = SSAParse.parseTime(self.__subtitle[x]["End"])
+				timeEndNew = float(timeEnd["hours"]*3600)+float(timeEnd["minutes"]*60)+timeEnd["seconds"]
+				self.__subtitle[x]["End"] = timeEndNew
+
+
+	def __getStyleIndex(self, name):
+		for x in xrange(0,len(self.__styles)):
+			if(self.__styles[x]["Name"]==name):
+				return x
+		raise ValueError("Style name not found")
+
+	def defineDefaultStyle(self, dict):
+		try:
+			index = self.__getStyleIndex("Default")
+		except ValueError:
+			try:
+				dict["Name"] = "Default"
+				self.__styles.append(dict)
+			except:
+				raise
+
+	def convertColor(self, type):
+		colorProp = ["PrimaryColour", "SecondaryColour", "TertiaryColour", "BackColour"]
+		if(type==self.HEXADECIMAL):
+			for x in xrange(0, len(self.__styles)):
+				for colorOne in colorProp:
+					dec = self.__styles[x][colorOne]
+					if(dec.find("&H")!=-1):
+						continue
+					hexa = hex(int(dec))
+					result = hexa.split("0x")[1]
+					if(len(result)==7):
+						result = "0"+result
+					while(len(result)<6):
+						result = "0"+result
+					result = "&H"+result
+					self.__styles[x][colorOne] = result
+					return
+		elif(type==self.DECIMAL):
+			for x in xrange(0, len(self.__styles)):
+				for colorOne in colorProp:
+					dec = self.__styles[x][colorOne]
+					if(dec.find("&H")==-1):
+						continue
+					dec = dec.split("&H")[1]
+					if(dec.find("&")!=-1):
+						dec = dec.split("&")[0]
+					result = int(dec, 16)
+					self.__styles[x][colorOne] = result
+					return
+
+	@staticmethod
+	def parseTime(time):
+		try:
+			dict = {}
+			times = time.split(":")
+			dict["hours"] = int(times[0])
+			dict["minutes"] = int(times[1])
+			dict["seconds"] = float(times[2])
+			return dict
+		except:
+			raise
